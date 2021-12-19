c65a2d7ff8f9fbe8f6fedef9ee856da7ded5ce07
Miles Possing <7330275+milespossing@users.noreply.github.com>
2021-12-19 11:42:51 -0800

    Updating for custom zsh changes

--- zsh-custom/alias.zsh
+++ zsh-custom/alias.zsh
@@ -1,3 +1,2 @@
 alias dc="docker compose"
 alias vim=nvim
-alias code="$WINHOME/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code"
---
+++ zsh-custom/plugins/my-wsl/my-wsl.plugin.zsh
@@ -0,0 +1,3 @@
+export WINHOME=/mnt/c/Users/{profileName}
+alias code="$WINHOME/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code"
+
