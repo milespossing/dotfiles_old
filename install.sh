540f7151a6bbf1cef3e3610494f5380c8a3c2edf
Miles Possing <milespossing@VonNeumann.localdomain>
2022-04-23 13:22:05 -0700

    Updates

--- install.sh
+++ install.sh
@@ -11,18 +11,7 @@ ln -f zsh/omz.zshrc $HOME/.zshrc
 # vim                     #
 ###########################
 
-echo "vim"
-mkdir -p $HOME/.config/nvim
-mkdir -p $HOME/.vim/bundle
-ln -f .vimrc $HOME/.vimrc
-ln -f .ideavimrc $HOME/.ideavimrc
-echo "source $HOME/.vimrc" > $HOME/.config/nvim/init.vim
-if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
-	echo "Cloning vundle"
-	git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-fi
-vim +PlugInstall +qall
-
+./install_vim.sh
 
 ###########################
 # tmux                    #
