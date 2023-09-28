d89f86c4b06279999b9896395371bf2324c355f8
Miles Possing <7330275+milespossing@users.noreply.github.com>
2023-09-28 14:55:07 -0700

    Adding browser support for wsl

--- config/doom/config.el
+++ config/doom/config.el
@@ -30,5 +30,14 @@
 ;; projectile
 (setq projectile-project-search-path '("~/src"))
 
+(when (and (eq system-type 'gnu/linux)
+           (string-match
+            "Linux.*Microsoft.*Linux"
+            (shell-command-to-string "uname -a")))
+  (setq
+   browse-url-generic-program  "/mnt/c/Windows/System32/cmd.exe"
+   browse-url-generic-args     '("/c" "start")
+   browse-url-browser-function #'browse-url-generic))
+
 ;; local
 (load! "local")
